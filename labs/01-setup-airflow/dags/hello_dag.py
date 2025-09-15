7d283ccc7e2e48e0c7ab50926e39322c1c0a55c1
Ana María López Moreno <darkanita@gmail.com>
2025-09-15 12:51:40 -0500

    Create hello_dag.py with a simple BashOperator

--- labs/01-setup-airflow/dags/hello_dag.py
+++ labs/01-setup-airflow/dags/hello_dag.py
@@ -1,1 +1,18 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+# Definición del DAG
+with DAG(
+    dag_id="hello_dag",
+    start_date=datetime(2025, 1, 1),
+    schedule="@daily",
+    catchup=False,
+    tags=["example", "intro"],
+) as dag:
+
+    hello = BashOperator(
+        task_id="hello_task",
+        bash_command="echo 'Hello, Airflow!'"
+    )
 
